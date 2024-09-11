bdb98f30595c0d7a198805db149edebcc72ff94a
Munna <ibnesinamunna@gmail.com>
2024-09-11 15:40:28 +0600

    php code

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+  /* <h1>My first PHP Page</h1>
+
+     <?php 
+        echo "hello World! "; 
+         echo "<br>";
+
+    $color = "blue";
+
+    echo "My car is", " ", $color;
+
+     echo "<br>";
+
+       echo "wellcome home!";
+      
+       echo "<br";
+
+       $age = 5;
+       $name = "sumon";
+
+       echo "amar boyos" ;
+
+
+    ?> 
+
+</body>
+</html>
