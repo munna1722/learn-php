936896d5f3a8f263d0dd876f68ac1caa48de9ec1
Munna <ibnesinamunna@gmail.com>
2024-09-11 16:40:21 +0600

    if else code

--- index.php
+++ index.php
@@ -7,4 +7,22 @@
       echo "Sorry! baby now not day,";
    }
  
+   echo "<br>";
+   $age = 70;
+
+    if ( $age = 70) {
+      echo"yes this age me ";
+    } else{
+      echo " sorry your age is 70, this age not your ";
+    }
+
+    echo "<br>";
+
+    $age = 70;
+
+    if( $age > 40){
+      echo "me";
+    }else{
+      echo "Not Me";
+    }
  ?>
