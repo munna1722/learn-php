286dc2e7d47b315f25d6322211dcc563620786a2
Munna <ibnesinamunna@gmail.com>
2024-09-13 01:02:38 +0600

    echo

---
+++ echoprint.php
@@ -0,0 +1,31 @@
+<?php
+
+echo "Hello baby";
+echo "<br>";
+// same code as 
+echo ("hello baby");
+echo '<br>';
+
+echo "<h2> i love this code php</h2>";
+echo "<br>"; //php use to html all heading you try it
+echo "<h3> i love this code php</h3>";
+
+echo "<br>";
+echo "This", "string","  ", "was", "made", "with multiple parameter";
+echo "<br>";
+
+
+$txt1 = "Learn PHP";
+$txt2 = "i love php you can do it";
+
+
+echo "<h2>$txt1</h2>";
+echo "<p style='color:blue;'>Study PHP at <a href='#'>$txt2</a></p>";
+// how to use php css, 
+// iccha moto css use kora  jabe kono somossas nai ,,
+echo "<h3>$txt2 <a style='color:red; font-size:45px;' href='#'> Go There</a></h3>";
+echo "<br>";
+
+echo '<h2>' . $txt1 . '</h2>';
+echo '<p> study PHP at' . $txt2 . '</p>';
+?>
