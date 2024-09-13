df83382d217980b770ae979241e906ec18c81e3e
Munna <ibnesinamunna@gmail.com>
2024-09-13 11:21:32 +0600

    H/W ja sob somoy mone rakhta hobe

---
+++ datatype.php
@@ -0,0 +1,16 @@
+<?php
+// php supports the following data types
+//  *string 
+//  *Integer
+//  *Float (floating point numbers-also called double)
+//  *Boolean
+//  *Array
+//  *Object
+//  *NULL
+//  *Resourcde
+// ai bisoy gulo sob smy mone rakta hobe 
+
+
+
+
+?>
