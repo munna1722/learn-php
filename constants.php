cab64ea3b2159403c45caad83127af41af27d3c2
Munna <ibnesinamunna@gmail.com>
2024-09-17 22:52:55 +0600

    magic constants __CLASS__

--- constants.php
+++ constants.php
@@ -1,31 +1,11 @@
 <?php
-//define (name, value);
+class Fruits {
+    public function myValue(){
+      return __CLASS__;
+    }
+  }
+  $kiwi = new Fruits();
+  echo $kiwi->myValue();
 
-define("GREETING", "Welcome to my php");
-echo GREETING;
-echo "<br>";
-
-
-const mycar = "tata";
-echo mycar;
-
-//creat an Array constant:
-
-define("car", [
-    "Alfa Romeo",
-    "BMW",
-    "Toyota"
-]);
-
-echo car[0];
-echo "<br>";
-
-
-define ("greeting", "welcome to my php"); // ai function a return korba (wellcome to my php)
-
-function myTest(){ //(myTest) ai ta function ar name
-    echo greeting;
-}
-
-myTest() // (myTest) ai ta function call kora hoycha 
+//echo __DIR__;// file ar jonno use kora hoy
 ?>
