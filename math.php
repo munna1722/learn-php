d1edc280ef005bfa4a184310d690d0a5de08adfa
Munna <ibnesinamunna@gmail.com>
2024-09-17 22:20:07 +0600

    php math

---
+++ math.php
@@ -0,0 +1,32 @@
+<?php
+
+echo (pi());
+echo "<br>";
+
+echo (min (0, 150, 30, 20, -8, -200) );
+echo "<br>";
+
+echo (max(0, 150, 30, 35, -8, -200));
+echo "<br>";
+
+echo(abs(-6.7));
+echo "<br>";
+
+echo (sqrt(64));
+echo"<br>";
+
+echo (round(0.78));
+echo "<br>";
+echo (round(0.34));
+echo "<br>";
+
+echo (rand());
+echo "<br>";
+
+echo(rand(10, 100));
+echo "<br>";
+
+
+
+
+?>
