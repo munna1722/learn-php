91a7caa5f59bcba9a46af79bf267755264af634d
Munna <ibnesinamunna@gmail.com>
2024-09-17 22:57:06 +0600

    function

--- constants.php
+++ constants.php
@@ -1,11 +1,18 @@
-<?php
-class Fruits {
-    public function myValue(){
-      return __CLASS__;
-    }
-  }
-  $kiwi = new Fruits();
-  echo $kiwi->myValue();
+ <?php
+// class Fruits {
+//     public function myValue(){
+//       return __CLASS__;
+//     }
+//   }
+//   $kiwi = new Fruits();
+//   echo $kiwi->myValue();
 
 //echo __DIR__;// file ar jonno use kora hoy
+// echo __FILE__;
+
+function myValue(){
+    return __FUNCTION__;
+} 
+echo myValue();
+
 ?>
