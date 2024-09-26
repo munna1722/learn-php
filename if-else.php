52834807cc5ab315470c1b36c01e6dd82c443cb0
Munna <ibnesinamunna@gmail.com>
2024-09-27 00:14:20 +0600

    if else operator

---
+++ if-else.php
@@ -0,0 +1,18 @@
+<?php
+if (5 > 3) {
+  echo "Have a good day!";
+}
+
+$t = 14;
+
+if ($t == 14) {
+  echo "Have a good day!";
+}
+
+$x = 100;  
+$y = 50;
+
+if ($x <> $y) {
+  echo "$x is not equal to $y";
+}
+?>
