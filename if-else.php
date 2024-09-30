ef692eccd4a2a5ddf72b51d98b8fc573336abbc1
Munna <ibnesinamunna@gmail.com>
2024-09-30 16:31:18 +0600

    baby

--- if-else.php
+++ if-else.php
@@ -15,4 +15,33 @@ $y = 50;
 if ($x <> $y) {
   echo "$x is not equal to $y";
 }
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$xname  = "ami Asif";
+$yname  = "ami mithila";
+$fullname = "$xname and $yname";
+
+$husbent = 30;
+$wife    = 20;
+
+$xhusbent = 5;
+$ywife    = 5;
+$baby     = 5;
+
+if ($husbent < $wife) {
+    echo "$xname biya korbo";
+} 
+elseif ($xhusbent > $ywife) {
+    echo "$xname o $yname biya korte pari ";
+} 
+elseif ($xhusbent > $baby) {
+  echo "$fullname  amader baby hoycha  ";
+} 
+else {
+    echo " $yname na ai ta somvob na";
+}
 ?>
