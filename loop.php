91c4e1abf634cb27139a3ac8daa25275ecf0648d
Munna <ibnesinamunna@gmail.com>
2024-12-09 14:21:13 +0600

    loop

--- loop.php
+++ loop.php
@@ -1,12 +1,28 @@
 <?php
 
-$x = 1;
-while ($x < 10) {
-    echo $x;
-    echo "<br>";
-    $x++;
-}
+// $x = 1;
+// while ($x < 10) {
+//     echo $x;
+//     echo "<br>";
+//     $x++;
+// }
 
+// $num = 1;
+// while ($num < 6) {
+//     if ( $num == 3 ) break;
+
+//     echo $num ;
+//     $num++; 
+// }
+
+
+// $num = 0;
+// while ($num < 6) {
+//     $num++;
+
+//     if ($num == 3) continue;
+//     echo $num;
+// }
 
 
 
