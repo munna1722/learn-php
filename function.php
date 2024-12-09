aff7ba606a75fa91d3079ae051259ab563f41421
Munna <ibnesinamunna@gmail.com>
2024-12-09 14:10:35 +0600

    php loop

--- function.php
+++ function.php
@@ -1,15 +1,24 @@
 <?php
-function myphp($fname) {
-    echo "$fname rafsana";
+// function myphp($fname) {
+//     echo "$fname rafsana";
+// }
+// myphp("jani");
+// echo "<br>";
+// myphp("hege");
+// echo "<br>";
+// myphp("stale");
+// echo "<br>";
+// myphp("Borge");
+// echo "<br>";
+// myphp("kai jim")
+
+
+function setheight ($minhight = 50) {
+    echo "The height is : $minhight <br>";
 }
-myphp("jani");
-echo "<br>";
-myphp("hege");
-echo "<br>";
-myphp("stale");
-echo "<br>";
-myphp("Borge");
-echo "<br>";
-myphp("kai jim")
+
+setheight(350);
+setheight();
+setheight(139);
 
 ?>
